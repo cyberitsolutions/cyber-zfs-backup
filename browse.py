462c2e28f418187c1b63efb0031cf3863777454f
Peter Wright <pete@cybersource.com.au>
2008-12-19 12:39:33 +1100

    Initial import.

---
+++ browse.py
@@ -0,0 +1,112 @@
+
+import sys
+import os
+import datetime
+import time
+from os.path import join
+
+######################################################################
+# Exceptions.
+
+class NotDirectory(Exception):
+    pass
+
+######################################################################
+# Functions.
+
+def getlsize(path):
+    return os.lstat(path)[6]
+
+def getlmtime(path):
+    return os.lstat(path)[8]
+
+def walk_from(start_dir):
+    for root, dirs, files in os.walk(start_dir):
+        print root, "consumes",
+        print sum([getlsize(join(root, name)) for name in files]),
+        print "bytes in", len(files), "non-directory files"
+        #if 'CVS' in dirs:
+        #    dirs.remove('CVS')  # don't visit CVS directories
+
+def file_type(filepath):
+    if os.path.islink(filepath):
+        return 'link'
+    if os.path.isfile(filepath):
+        return 'file'
+    if os.path.isdir(filepath):
+        return 'dir'
+    return 'special'
+
+# These three always show the directories first.
+
+def cmp_filespec_name(a, b):
+    if a['type'] == 'dir' and b['type'] != 'dir':
+        return -1
+    if b['type'] == 'dir' and a['type'] != 'dir':
+        return 1
+    return cmp(a['name'], b['name'])
+
+def cmp_filespec_size(a, b):
+    if a['type'] == 'dir' and b['type'] != 'dir':
+        return -1
+    if b['type'] == 'dir' and a['type'] != 'dir':
+        return 1
+    return cmp(a['size'], b['size'])
+
+def cmp_filespec_mtime(a, b):
+    if a['type'] == 'dir' and b['type'] != 'dir':
+        return -1
+    if b['type'] == 'dir' and a['type'] != 'dir':
+        return 1
+    return cmp(a['mtime'], b['mtime'])
+
+filespec_cmp = {
+    'name' : cmp_filespec_name,
+    'size' : cmp_filespec_size,
+    'mtime' : cmp_filespec_mtime
+}
+
+def make_filespec(path, name=False):
+    if not name:
+        name = os.path.basename(path)
+    return {
+        'name' : name,
+        'path' : path,
+        'type' : file_type(path),
+        'size' : getlsize(path),
+        'mtime' : getlmtime(path)
+    }
+
+# os.path.islink
+# os.path.isdir
+# os.path.isfile
+def get_dir_contents(dir, sort_by="name", parent=True):
+    if not os.path.isdir(dir):
+        raise NotDirectory("%s is not a directory." % ( dir ))
+
+    dir_contents = os.listdir(dir)
+    contents = []
+    for fname in dir_contents:
+        abspath = os.path.abspath(os.path.join(dir, fname))
+        contents.append(make_filespec(abspath))
+
+    contents.sort(filespec_cmp[sort_by])
+
+    if parent:
+        parent_dir_path = os.path.abspath(os.path.join(dir, '..'))
+        parent_dir = make_filespec(parent_dir_path, name="PARENT DIRECTORY")
+        contents.insert(0, parent_dir)
+
+    return contents
+
+
+######################################################################
+# Main.
+
+#start_dir = os.path.abspath(sys.argv[1])
+#print "Starting with %s ..." % ( start_dir )
+#walk_from(start_dir)
+#print "Directory contents:\n"
+#show_dir_contents(start_dir)
+
+
---
+++ html.py
@@ -0,0 +1,86 @@
+# This is a very very _very_ simple HTML library.
+#
+# Usage:
+#
+# html(head(title("Title"))
+#     + body(h1("Heading")
+#     + p("Paragraph")
+#     + hr()
+#     + p("Footer", att="style='font-size:0.9em'")))
+#
+# => "<html><head><title>Title</title></head><body><h1>Heading</h1><p>Paragraph</p><hr></hr><p style='font-size:0.9em'>Footer</p></body></html>"
+#
+# If you think it's too simple to use, feel free to go away and use
+# some fancy templating "solution" like Mako or Cheetah. Have fun.
+
+
+######################################################################
+# Default doctype.
+html_4_01_strict = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">'
+
+
+######################################################################
+# HTML tag handler.
+def tag(tagname, items, attributes='', close=True):
+    if attributes != '': attributes = ' ' + attributes
+    start = '<' + tagname + attributes + '>'
+    end = ''
+    if close: end = '</' + tagname + '>'
+    retval = ''
+    if type(items) != type([]):
+        items = [items]
+    for i in items:
+        retval += "%s%s%s" % ( start, i, end )
+    return retval
+
+
+######################################################################
+# HTML tag wrapper functions.
+
+# Formatting.
+def a(data='', att=''): return tag('a', data, att)
+def b(data='', att=''): return tag('b', data, att)
+def i(data='', att=''): return tag('i', data, att)
+def em(data='', att=''): return tag('em', data, att)
+def strong(data='', att=''): return tag('em', data, att)
+
+def p(data='', att=''): return tag('p', data, att)
+def br(data='', att=''): return tag('br', data, att, close=False)
+
+def h1(data='', att=''): return tag('h1', data, att)
+def h2(data='', att=''): return tag('h2', data, att)
+def h3(data='', att=''): return tag('h3', data, att)
+def h4(data='', att=''): return tag('h4', data, att)
+
+# Structural.
+def html(data='', att='', doctype=html_4_01_strict):
+    return doctype + tag('html', data, att)
+def head(data='', att=''): return tag('head', data, att)
+def body(data='', att=''): return tag('body', data, att)
+def title(data='', att=''): return tag('title', data, att)
+def div(data='', att=''): return tag('div', data, att)
+def span(data='', att=''): return tag('span', data, att)
+def link(data='', att=''): return tag('link', data, att)
+
+# Tables.
+def table(data='', att=''): return tag('table', data, att)
+def tr(data='', att=''): return tag('tr', data, att)
+def td(data='', att=''): return tag('td', data, att)
+def th(data='', att=''): return tag('th', data, att)
+
+# Misc.
+def hr(data='', att=''): return tag('hr', data, att, close=False)
+def img(data='', att=''): return tag('img', data, att, close=False)
+
+# Lists.
+def ul(data='', att=''): return tag('ul', data, att)
+def ol(data='', att=''): return tag('ol', data, att)
+def li(data='', att=''): return tag('li', data, att)
+
+# Forms.
+def form(data='', att=''): return tag('form', data, att)
+def input(data='', att=''): return tag('input', data, att)
+def textarea(data='', att=''): return tag('textarea', data, att)
+def option(data='', att=''): return tag('option', data, att)
+def select(data='', att=''): return tag('select', data, att)
+
