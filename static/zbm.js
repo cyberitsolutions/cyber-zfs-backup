3d5fc72c9ab197249a9239073d311ac2f1d77b10
Peter Wright <pete@cybersource.com.au>
2008-12-24 17:11:29 +1100

    Implemented a working extractValue and sorting setup.

--- static/zbm.js
+++ static/zbm.js
@@ -1,8 +1,31 @@
 
 $(document).ready(
     function() {
+        var extractValue = function(node) {
+            var child = node.childNodes[0];
+            if (!child) { return ''; }
+            if (child.nodeName == "#text") {
+                return node.innerHTML;
+            } else {
+                var value = child.getAttribute("value");
+                if (value) {
+                    return value;
+                } else {
+                    return child.innerHTML;
+                }
+            }
+        };
         $("table.browsedir").tablesorter({
-            headers: { 4: { sorter: false } }
+            //debug: true,
+            textExtraction: extractValue,
+            headers: {
+                0: { sorter: "text" },
+                1: { sorter: "digit" },
+                2: { sorter: "text" },
+                3: { sorter: "text" },
+                4: { sorter: "isoDate" },
+                5: { sorter: false }
+            },
         });
     }
 );
