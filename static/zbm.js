986fe864cff1a5725908455a83a2b1de0887b2d3
Peter Wright <pete@cybersource.com.au>
2008-12-29 12:51:47 +1100

    Moved code in the $(document).ready function out to named functions. * function extractValue * function setup_browsedir_sort

--- static/zbm.js
+++ static/zbm.js
@@ -1,33 +1,39 @@
 
+function extractValue(node) {
+    var child = node.childNodes[0];
+    if (!child) { return ''; }
+    if (child.nodeName == "#text") {
+        return node.innerHTML;
+    } else {
+        var value = child.getAttribute("value");
+        if (value) {
+            return value;
+        } else {
+            return child.innerHTML;
+        }
+    }
+}
+
+function setup_browsedir_sort() {
+    $("table.browsedir").tablesorter({
+        //debug: true,
+        textExtraction: extractValue,
+        headers: {
+            0: { sorter: false },
+            1: { sorter: "text" },
+            2: { sorter: "digit" },
+            3: { sorter: "text" },
+            4: { sorter: "text" },
+            5: { sorter: "isoDate" },
+            6: { sorter: false }
+        },
+    });
+}
+
+
 $(document).ready(
     function() {
-        var extractValue = function(node) {
-            var child = node.childNodes[0];
-            if (!child) { return ''; }
-            if (child.nodeName == "#text") {
-                return node.innerHTML;
-            } else {
-                var value = child.getAttribute("value");
-                if (value) {
-                    return value;
-                } else {
-                    return child.innerHTML;
-                }
-            }
-        };
-        $("table.browsedir").tablesorter({
-            //debug: true,
-            textExtraction: extractValue,
-            headers: {
-                0: { sorter: false },
-                1: { sorter: "text" },
-                2: { sorter: "digit" },
-                3: { sorter: "text" },
-                4: { sorter: "text" },
-                5: { sorter: "isoDate" },
-                6: { sorter: false }
-            },
-        });
+        setup_browsedir_sort();
     }
 );
 
