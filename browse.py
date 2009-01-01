130039ece147a49de8d88c1fcd255988e244025e
Peter Wright <pete@cybersource.com.au>
2009-01-01 15:33:01 +1100

    Removed the commented out BadPrefixError exception.

--- browse.py
+++ browse.py
@@ -14,12 +14,6 @@ import chroot
 ######################################################################
 # Exceptions.
 
-#class BadPrefixError(Exception):
-#    def __init__(self, value=""):
-#        self.value = value
-#    def __str__(self):
-#        return self.value
-#
 class InaccessiblePathError(Exception):
     def __init__(self, value=""):
         self.value = value
