e9584c5cf7a21875c3326a9ecc2175565be92cff
Peter Wright <pete@cybersource.com.au>
2009-01-04 18:16:27 +1100

    Added simple database-access module. Uses the pattern from http://tools.cherrypy.org/wiki/Databases .

---
+++ db.py
@@ -0,0 +1,38 @@
+
+import cherrypy
+import psycopg2
+
+
+def connect(thread_index):
+    # Create a connection and store it in the current thread.
+    # No, we don't use the thread_index var.
+    cherrypy.thread_data.db = psycopg2.connect(database='zbm', host='localhost', user='zbm', password='zbm')
+
+# Just execute, ignore the result.
+def do(sql, vars=None):
+    # Get cursor.
+    c = cherrypy.thread_data.db.cursor()
+    c.execute(sql, vars=vars)
+    # Close cursor.
+    c.close()
+
+# Return a list of results.
+def get(sql, vars=None):
+    # Get cursor.
+    c = cherrypy.thread_data.db.cursor()
+    c.execute(sql, vars=vars)
+    result = c.fetchall()
+    # Close cursor.
+    c.close()
+    return result
+
+# Return a single result.
+def get1(sql, vars=None):
+    # Get cursor.
+    c = cherrypy.thread_data.db.cursor()
+    c.execute(sql, vars=vars)
+    result = c.fetchone()
+    # Close cursor.
+    c.close()
+    return result
+
