4ba2618a5082180c49537998b7adf2212ba4973c
Peter Wright <pete@cybersource.com.au>
2009-01-25 22:50:31 +1100

    Extracted database shellscript config into separate file.

---
+++ sql/db-cfg.sh
@@ -0,0 +1,9 @@
+# The zbm database name.
+DB_NAME="zbm"
+
+# User responsible for creating zbm database.
+SUPER_USER="pete"
+
+# Grant all necessary privileges to the zbm user.
+ZBM_USER="zbm"
+
--- sql/grant.sh
+++ sql/grant.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# The zbm database name.
-DB_NAME="zbm"
-
-# User responsible for creating zbm database.
-SUPER_USER="pete"
-
-# Grant all necessary privileges to the zbm user.
-ZBM_USER="zbm"
+# Configuration.
+. ./db-cfg.sh
 
 # Table on which to grant necessary privileges.
 TABLES="companies shares users restores restore_files"
