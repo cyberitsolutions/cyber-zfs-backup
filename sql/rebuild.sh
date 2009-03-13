c7bde0397d0cb5cab123c24662e7e928d866ee3a
Peter Wright <pete@cybersource.com.au>
2009-03-13 11:26:44 +1100

    Modified format of schema/data restore calls.

--- sql/rebuild.sh
+++ sql/rebuild.sh
@@ -8,8 +8,8 @@ dropdb -U $SUPER_USER $DB_NAME
 createdb -U $SUPER_USER $DB_NAME
 
 # Set up tables and initial data.
-psql -U $SUPER_USER $DB_NAME < db.sql
-psql -U $SUPER_USER $DB_NAME < db-data.sql
+psql -U $SUPER_USER -f db.sql $DB_NAME
+psql -U $SUPER_USER -f db-data.sql $DB_NAME
 
 # Grant privileges to the zbm user.
 ./grant.sh
