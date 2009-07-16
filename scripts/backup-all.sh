5b66b0093cb080cf617ac29c3b2f4ec7ec40f9ce
Peter Wright <pete@cybersource.com.au>
2009-07-16 13:14:44 +1000

    it might be a good idea to include error  in the daily log emails...

--- scripts/backup-all.sh
+++ scripts/backup-all.sh
@@ -5,14 +5,17 @@
 IFS=$'\n'
 
 cd /tank/hosted-backup
-rm -f z-backup-all{,-legacy}.{out,err}
+rm -f z-backup-output.txt
 # config files containing RSA private keys for rsync+ssh clients
 for conf in $(cd config && grep -l -- '-----BEGIN RSA PRIVATE KEY-----' *)
 do
-  ./bin/backup-share.pl "$conf" >> z-backup-all.out 2>> z-backup-all.err
+  ./bin/backup-share.pl "$conf" 2>&1 >> z-backup-output.txt
 done
 for legacy_conf in $(cd config-legacy && ls -1)
 do
-  ./bin/backup-share-legacy.pl "$legacy_conf" >> z-backup-all-legacy.out 2>> z-backup-all-legacy.err
+  ./bin/backup-share-legacy.pl "$legacy_conf" 2>&1 >> z-backup-output.txt
 done
+
+(cat z-backup-output.txt; head -100 backups/*/*.err) | /usr/bin/mailx -s 'zhug backup logs' hosted-backups@cybersource.com.au
+
 mv backups/*/*.{out,err} logs
