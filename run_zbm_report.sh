ee425af32e485967df91699c255c63e668ce5dcc
Peter Wright <pete@cybersource.com.au>
2009-12-03 18:03:13 +1100

    Simple kludge to easily run the two different reports and send with different Subject header fields.

--- run_zbm_report.sh
+++ run_zbm_report.sh
@@ -4,5 +4,12 @@ export LD_LIBRARY_PATH=/usr/postgres/8.2/lib:$LD_LIBRARY_PATH
 
 cd /export/home/cyber/src/zbm
 
-./zbm_report 2> /dev/null | cat zbm_report_header.txt - | mail hosted-backups@cybersource.com.au
+header=zbm_report_header.txt
+steve=""
+if [ -n "$1" ]; then
+    steve="steve"
+    header=zbm_report_header_steve.txt
+fi
+
+./zbm_report $steve 2> /dev/null | cat "$header" - | mail hosted-backups@cybersource.com.au
 
