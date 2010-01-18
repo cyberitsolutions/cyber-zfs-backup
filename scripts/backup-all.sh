774cba6f2d77c580b0df963192e96160e8cd0606
Peter Wright <pete@cybersource.com.au>
2010-01-18 15:30:47 +1100

    kill old rsyncs before starting a new daily run

--- scripts/backup-all.sh
+++ scripts/backup-all.sh
@@ -6,11 +6,18 @@ IFS=$'\n'
 
 cd /tank/hosted-backup
 rm -f z-backup-output.txt
+
+# nuke any existing rsync processes
+# XXX this sucks, and should only be killing *our* rsync processes, but
+# XXX since we don't save their PIDs we just hit everything with a rock.
+/usr/bin/pkill rsync; sleep 30; /usr/bin/pkill -9 rsync
+
 # config files containing RSA private keys for rsync+ssh clients
 for conf in $(cd config && grep -l -- '-----BEGIN RSA PRIVATE KEY-----' *)
 do
   ./bin/backup-share.pl "$conf" 2>&1 >> z-backup-output.txt &
 done
+# config files containing rsync passwords for pure rsync clients
 for legacy_conf in $(cd config-legacy && ls -1)
 do
   ./bin/backup-share-legacy.pl "$legacy_conf" 2>&1 >> z-backup-output.txt &
