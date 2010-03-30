43ce6fa461f5d406fd471f7afaac568f0a4bf448
Peter Wright <pete@cybersource.com.au>
2010-03-30 17:28:37 +1100

    Bandwidth report stuff.

---
+++ run_bandwidth_report.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=/usr/postgres/8.2/lib:$LD_LIBRARY_PATH
+
+cd /export/home/cyber/src/zbm
+
+header=bandwidth_report_header.txt
+
+./restore_bandwidth_report 2> /dev/null | cat "$header" - | mail hosted-backups@cybersource.com.au
+
