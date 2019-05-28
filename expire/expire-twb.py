2537523f65ef87195e65b8a00cc63990a20e4392
Trent W. Buck <trentbuck@gmail.com>
2019-05-28 17:09:27 +1000

    fixup! ground-up rewrite with testing (so far)
    I'm pretty confident the test version actually works now.

--- expire/expire-twb.py
+++ expire/expire-twb.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
-import datetime
-import os
-import time
-import pathlib
 import logging
-import subprocess
 
 
-# NOTE: we use arrow (not datetime) because datetime.timedelta can't do weeks/months/years!
+# NOTE: we use arrow (not datetime) because
+#       datetime.timedelta can't do weeks/months/years!
 # NOTE: we use click (not argparse) gives us easier input validation.
 import arrow                    # https://arrow.readthedocs.io
 import click                    # https://click.palletsprojects.com
+
 
 @click.command()
 @click.argument('path',
@@ -41,18 +38,27 @@ def main(path, days, weeks, months, years, verbose):
     # More importantly, that means that if there are missing backups,
     # or multiple backups per day, we won't get confused and over-delete.
     # ...I think.
-
     pass
-
-
 
 
 def test():
     logging.basicConfig(level=logging.INFO)
     import random
-    import pprint
+
     now = arrow.now()
+
+    def make_snapshot():
+        # Our current backup code names snapshots (NB: Z = UTC)
+        #   tank/foo/bar@2019-05-27T14:00:00Z
+        # For this test, we'll just use the part after '@'.
+        # arrow.get() understands that format without help.
+        snapshots.append(
+            str(now.to('UTC').strftime('%Y-%m-%dT%H:%M:%SZ')))
+
+    # This variable stores our fake view of "the world", i.e.
+    # just a list of snapshot names (as str objects).
     snapshots = []
+
     for i in range(1000):
         # Go forward 1 day, to simulate the progress of time.
         now = now.floor('day').shift(days=1)
@@ -61,10 +67,8 @@ def test():
                           minute=random.randrange(60))
 
         # Simulate a backup.  It succeeds 80% of the time.
-        if random.random() <= 0.80:
-            # Our current backup code names snapshots (NB: Z = UTC)
-            #   tank/foo/bar@2019-05-27T14:00:00Z
-            snapshots.append(str(now.to('UTC').strftime('%Y-%m-%dT%H:%M:%SZ')))
+        if random.random() <= 0.95:
+            make_snapshot()
             logging.info('%s backup done', now)
         else:
             logging.warning('%s backup failed', now)
@@ -79,37 +83,33 @@ def test():
 
         # Simulate a rare second manual backup done by the sysadmin.
         # This lets us test a few "multiple snaps per day".
-        if random.random() <= 0.05:
+        if random.random() <= 0.02:
             logging.warning('%s sysadmin made manual backup(s)', now)
             now = now.shift(minutes=300*random.random())
-            snapshots.append(str(now.to('UTC').strftime('%Y-%m-%dT%H:%M:%SZ')))
-            if random.random() <= 0.50:
-                now = now.shift(minutes=5*random.random())
-                snapshots.append(str(now.to('UTC').strftime('%Y-%m-%dT%H:%M:%SZ')))
-
-
-        # pprint.pprint(snapshots)
+            make_snapshot()
 
 
 def test_expire(now, snapshots):
     days, weeks, months, years = 7, 4, 12, 10
     snapshots.sort(key=arrow.get, reverse=True)  # most recent date first
 
+    before_len = len(snapshots)
+
     for prev_snapshot, snapshot in zip([None] + snapshots,
-                                           snapshots):
+                                       snapshots):
         prev_snapshot_ts = arrow.get(prev_snapshot) if prev_snapshot else None
         snapshot_ts = arrow.get(snapshot) if snapshot else None
         if prev_snapshot is None:
-            # First snapshot is ALWAYS kept
-            logging.debug('%s %s first snapshot is always kept', now, snapshot_ts)
+            logging.debug('%s %s first snapshot', now, snapshot_ts)
             keep = True
         elif snapshot_ts > now:
-            logging.warning('%s %s future snapshot detected!', now, snapshot_ts)
+            logging.warning('%s %s future snapshot!', now, snapshot_ts)
             keep = True
         elif snapshot_ts.floor('day') == now.floor('day'):
             logging.debug('%s %s KEEP because today', now, snapshot_ts)
             keep = True
-        ## NOTE: the next four branches are IDENTICAL except for the UNIT (day/week/...).
+
+        # NOTE: these branches are IDENTICAL except for day/week/&c.
         elif days:              # we're looking for the next day
             if prev_snapshot_ts.floor('day') == snapshot_ts.floor('day'):
                 # Day hasn't changed; keep looking
@@ -143,17 +143,50 @@ def test_expire(now, snapshots):
                 years = years - 1
                 keep = True
         else:
-            # We don't care about any more rotations, so just throw away anything else.
-            # FIXME: I think we should actually keep "infinite" years by default.
+            # We don't care about any more rotations, so
+            # just throw away anything else.
+            # FIXME: we should keep "infinite" years by default, and
+            #        raise an error in this case!
             keep = False
 
-        logging.info('%s %s %s (%s)', now, snapshot_ts, 'KEEP' if keep else 'KILL', snapshot_ts.humanize())
+        logging.info('%s %s %s (%s)',
+                     now,
+                     snapshot_ts,
+                     'keep' if keep else 'KILL',
+                     my_humanize(now, snapshot_ts))
         if keep:
             pass
         else:
             logging.debug('%s zfs destroy ...@%s', now, snapshot)
-            snapshots.remove(snapshot)  # edit the simulation, rather than actually deleting
+            # edit the simulation, rather than actually deleting
+            snapshots.remove(snapshot)
 
+    after_len = len(snapshots)
+    if before_len - after_len > 1:
+        logging.warning('%s multiple KILLs in this expiry!', now)
+
+
+# This bodge helps you examine test_expire's output.
+# It is something like arrow.Arrow.humanize().
+# It is something like postgresql age().
+def my_humanize(now: arrow.Arrow, ts: arrow.Arrow) -> str:
+    days_different = now.toordinal() - ts.toordinal()
+    s = 'today'
+    if days_different == 0:
+        return s
+    s += ' +' if days_different > 0 else ' -'
+    if days_different > 365:
+        s += ' {} years'.format(days_different//365)
+        days_different = days_different % 365
+    if days_different > 31:     # shitty approximation!
+        s += ' {} months'.format(days_different//31)
+        days_different = days_different % 31
+    if days_different > 7:     # shitty approximation!
+        s += ' {} weeks'.format(days_different//7)
+        days_different = days_different % 7
+    if days_different:     # shitty approximation!
+        s += ' {} days'.format(days_different)
+    return s
 
 
 test()
